92ebf2386def7d7a8e0eaee3b33ed1e7f3e9f5d3
Chaossub <jinxedprincess182@gmail.com>
2025-07-03 16:45:47 -0700

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,12 +1,8 @@
 from pymongo import MongoClient
+import certifi
 from config import MONGODB_URI
 
-mongo_client = MongoClient(
-    MONGODB_URI,
-    tls=True,
-    tlsAllowInvalidCertificates=False  # or True only if you absolutely trust the certs
-)
-
+mongo_client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 succubot_db = mongo_client["succubot"]
 
 # Collections:
