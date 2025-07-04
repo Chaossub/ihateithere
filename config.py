654189f57e2c4b4ff115f1fc555da951531ef3b3
Chaossub <jinxedprincess182@gmail.com>
2025-07-03 17:20:52 -0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGODB_URI = os.getenv("MONGODB_URI")
 
-# List of Telegram user IDs with full permissions
+# Your Telegram user ID (superuser)
 SUPERUSERS = [6964994611]
+
+# Updated, secure MongoDB connection string
+MONGODB_URI = "mongodb+srv://chaossunflowerbusiness321:Urmom420@cluster0.y0defvi.mongodb.net/succubot?retryWrites=true&w=majority&tls=true"
 
