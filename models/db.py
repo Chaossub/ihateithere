710584059fd6b609b1b819df0e1df3a59ad2d63b
Chaossub <jinxedprincess182@gmail.com>
2025-07-03 17:21:15 -0700

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,15 +1,9 @@
 from pymongo import MongoClient
 from config import MONGODB_URI
 
-# Initialize MongoDB client
-mongo_client = MongoClient(MONGODB_URI, tls=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000)
-
-# Main bot database
-db = mongo_client["succubot"]
+# Create Mongo client and connect to DB
+mongo_client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
+succubot_db = mongo_client["succubot"]
 
 # Collections
-feds_col = db["federations"]
-users_col = db["users"]
-summon_col = db["summon"]
-xp_col = db["xp"]
-warns_col = db["warnings"]
+feds_col = succubot_db["federations"]
