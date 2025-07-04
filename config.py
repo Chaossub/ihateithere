a90f25bad755bbbec309261227a91fd6de8d635e
Chaossub <jinxedprincess182@gmail.com>
2025-07-03 17:39:53 -0700

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# MongoDB URI (with correct format)
-MONGODB_URI = "mongodb+srv://chaossunflowerbusiness321:Urmom420@cluster0.y0defvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# MongoDB connection string
+MONGODB_URI = (
+    "mongodb+srv://chaossunflowerbusiness321:Sunflower@cluster0.y0defvi.mongodb.net/"
+    "?retryWrites=true&w=majority&appName=Cluster0"
+)
 
-# Superusers (Telegram user IDs that have full control)
-SUPERUSERS = [6964994611]  # ← Your Telegram ID
-
-
+# Superusers (bot owner/admins with full access)
+SUPERUSERS = [6964994611]
